59a91375921660ff018b21f86a1a2a62f903371c
Oksana <oksana.barnasheva@gmail.com>
2018-07-29 21:07:04 +0300

    BuildControl widget

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from './BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
             </Aux>
         );
     }
